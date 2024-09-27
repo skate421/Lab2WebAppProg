64a64a42a186956b89e80fba076feea62e26cf42
Skate421 <SKATIE13@outlook.com>
2024-09-27 14:07:46 -0300

    commit1

---
+++ connect.js
@@ -0,0 +1,24 @@
+require('dotenv').config
+
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = process.env.MONGODB
+
+// Connect to your Atlas cluster
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to Atlas");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
